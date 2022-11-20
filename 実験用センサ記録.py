068090e36148ed9e411c66d2c71fb5b0a08b74e5
SHUNTAs <kingdomshin16@gmail.com>
2022-11-20 20:17:00 +0900

    save bno055 file

--- 実験用センサ記録.py
+++ 実験用センサ記録.py
@@ -58,7 +58,7 @@ class dataFileManager:
             
             fig, ax = plt.subplots()
             ax.set_xlabel('経過時間 [ms]')  # x軸ラベル
-            ax.set_ylabel('y')  # y軸ラベル
+            ax.set_ylabel('クォータニオン')  # y軸ラベル
             ax.set_title(str(sensor_num) +'_相対方位角') # グラフタイトル
             # ax.set_aspect('equal') # スケールを揃える
             ax.grid()            # 罫線
@@ -71,7 +71,8 @@ class dataFileManager:
             ax.legend(loc=0)    # 凡例
             fig.tight_layout()  # レイアウトの設定
             plt.savefig(self.location +'/センサ'+str(sensor_num) + '_' + '相対方位角') # 画像の保存
-            plt.show()
+            plt.clf()
+            plt.close()
             
         """角速度の描画"""
         for sensor_num in range(0,5):
@@ -82,7 +83,7 @@ class dataFileManager:
             
             fig, ax = plt.subplots()
             ax.set_xlabel('経過時間 [ms]')  # x軸ラベル
-            ax.set_ylabel('y')  # y軸ラベル
+            ax.set_ylabel('角速度 [rad/s]')  # y軸ラベル
             ax.set_title(str(sensor_num) + '_角速度') # グラフタイトル
             # ax.set_aspect('equal') # スケールを揃える
             ax.grid()            # 罫線
@@ -94,7 +95,8 @@ class dataFileManager:
             ax.legend(loc=0)    # 凡例
             fig.tight_layout()  # レイアウトの設定
             plt.savefig(self.location +'/センサ'+str(sensor_num) + '_' + '角速度') # 画像の保存
-            plt.show()
+            plt.clf()
+            plt.close()
         """線形加速度の描画"""
         for sensor_num in range(0,5):
             head_num = 8 + 14 * sensor_num
@@ -104,7 +106,7 @@ class dataFileManager:
             
             fig, ax = plt.subplots()
             ax.set_xlabel('経過時間 [ms]')  # x軸ラベル
-            ax.set_ylabel('y')  # y軸ラベル
+            ax.set_ylabel('線形加速度 [m/s^2]')  # y軸ラベル
             ax.set_title(str(sensor_num) + '_線形加速度') # グラフタイトル
             # ax.set_aspect('equal') # スケールを揃える
             ax.grid()            # 罫線
@@ -116,8 +118,39 @@ class dataFileManager:
             ax.legend(loc=0)    # 凡例
             fig.tight_layout()  # レイアウトの設定
             plt.savefig(self.location +'/センサ'+str(sensor_num) + '_' + '線形加速度') # 画像の保存
-            plt.show()
+            plt.clf()
+            plt.close()
+    
+def calibration_mode(DATA_LENGTH):
+    calibration_value = []
+    system_tag = '\033[31m'+'system'+'\033[0m'
+    gyro_tag = '\033[31m'+'gyro'+'\033[0m'
+    accel_tag = '\033[31m'+'accel'+'\033[0m'
+    magnet_tag = '\033[31m'+'mag'+'\033[0m'
+    try: 
+        time_data = judge_time_data()
+        #print(time_data)
+        calibration_value.append(time_data)
         
+        for num in range(DATA_LENGTH-1):
+            calibration_value.append(float(ser.readline().decode('utf-8').rstrip('\r\n')))
+        #print(calibration_value)
+        if calibration_value[11] ==3.0 and calibration_value[25] == 3.0 and calibration_value[39] == 3.0 and calibration_value[53] == 3.0 and calibration_value[67]  == 3.0:
+            system_tag = '\033[32m'+'system'+'\033[0m'
+        if calibration_value[12] ==3.0 and calibration_value[26] == 3.0 and calibration_value[40] == 3.0 and calibration_value[54] == 3.0 and calibration_value[68]  == 3.0:
+            gyro_tag = '\033[32m'+'gyro'+'\033[0m'    
+        if calibration_value[13] ==3.0 and calibration_value[27] == 3.0 and calibration_value[41] == 3.0 and calibration_value[55] == 3.0 and calibration_value[69]  == 3.0:
+            accel_tag = '\033[32m'+'accel'+'\033[0m'
+        if calibration_value[14] ==3.0 and calibration_value[28] == 3.0 and calibration_value[42] == 3.0 and calibration_value[56] == 3.0 and calibration_value[70]  == 3.0:
+            magnet_tag = '\033[32m'+'mag'+'\033[0m'
+                
+        print('['+ system_tag +','+ gyro_tag +',' + accel_tag +',' + magnet_tag +']') 
+        calibration_value = []
+    except:
+        print('読み込みエラー')
+
+    
+
 
 if __name__ == '__main__':
     
@@ -150,48 +183,68 @@ if __name__ == '__main__':
             
             if status == 's':
                 #ファイル名作成
-                positionTipes =input('センサ配置')
-                roadTipes = input('走行路面')
+                roadtypes =input('走行路面')
+                walktracks = input('直進or蛇行')
+                runspeed = input('歩行速度')
                 runTimes =input('走行回数')
                 now = datetime.datetime.now()
-                #ファイル作成
-                os.makedirs(csv_dir + now.strftime('%Y%m%d_%H%M%S'),exist_ok = True)
-                csvFileName = csv_dir + '/'+ now.strftime(('%Y%m%d_%H%M%S')) +'/' +positionTipes + '_' + roadTipes + '_' + runTimes + '.csv'
-                #ファイル展開      
-                csvf = open(csvFileName, 'w',newline="")
-                fwriter = csv.writer(csvf)
-                fwriter.writerow(csvColumn1)
-                fwriter.writerow(csvColumn3)                
-                print('記録中：', csvFileName)
+                if(roadtypes == 'キャリブレーション'):
+                    pass
+                else:
+                    #ファイル作成
+                    os.makedirs(csv_dir + now.strftime('%Y%m%d_%H%M%S'),exist_ok = True)
+                    csvFileName = csv_dir + '/'+ now.strftime(('%Y%m%d_%H%M%S')) +'/' +roadtypes + '_' +walktracks + '_' + runspeed + '_' + runTimes + '.csv'
+                    #ファイル展開      
+                    csvf = open(csvFileName, 'w',newline="")
+                    fwriter = csv.writer(csvf)
+                    fwriter.writerow(csvColumn1)
+                    fwriter.writerow(csvColumn3)                
+                    print('記録中：', csvFileName)
+                    
+                    #31行分を無視
+                    for initial_num in range(0,29):
+                        try:
+                            
+                            time_data = judge_time_data()
+                            #print(time_data)
+                            
+                        except:
+                            print('読み込みエラー')
             
             elif status == 'f':
-                
-                try:
-                    csvf.close()
-                    print('記録終了')
-                    '''図を作成するコード'''
-                    df_manager = dataFileManager(csvFileName, csv_dir + '/'+ now.strftime(('%Y%m%d_%H%M%S')) +'/')
-                    df_manager.make_picture()
-                except:
+                if roadtypes == 'キャリブレーション':
+                    print('キャリブレーション画面を閉じました')
                     pass
+                else:
+                    try:
+                        csvf.close()
+                        print('記録終了')
+                        '''図を作成するコード'''
+                        df_manager = dataFileManager(csvFileName, csv_dir + '/'+ now.strftime(('%Y%m%d_%H%M%S')) +'/')
+                        df_manager.make_picture()
+                    except:
+                        pass
         
         if status == 'f':
             pass
         
         elif status == 's':
+            if roadtypes == 'キャリブレーション':
+                calibration_mode(DATA_LENGTH)
                 
-            try:
+            else:
+                try:
+                    
+                    time_data = judge_time_data()
+                    #print(time_data)
+                    value.append(time_data)
+                    
+                    for data_num in range(DATA_LENGTH-1):
+                        value.append(float(ser.readline().decode('utf-8').rstrip('\r\n')))
                 
-                time_data = judge_time_data()
-                print(time_data)
-                value.append(time_data)
+                except:
+                    print('読み込みエラー')
                 
-                for data_num in range(DATA_LENGTH-1):
-                    value.append(float(ser.readline().decode('utf-8').rstrip('\r\n')))
-            
-            except:
-                print('読み込みエラー')
-            
-            print(value)
-            fwriter.writerow(value)
-            value = []
+                print(value)
+                fwriter.writerow(value)
+                value = []
